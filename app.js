bfb7854273c34976808280d0833a0590bdb5bd2b
Vincent Duong <vncntduong@gmail.com>
2015-11-03 21:41:11 -0800

    asdf 2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var DiversifyApp = require('./components/DiversifyApp');
+var _ = require('lodash');
 
 // Render the components, picking up where react left off on the server
 ReactDOM.render(
